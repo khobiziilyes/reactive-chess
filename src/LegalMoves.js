1b4f167488c5b2adc0c85e99f361386eda34ba47
Khobizi Ilyes <ilyes@yib.io>
2022-05-13 22:33:34 +0100

    reformat.

--- src/LegalMoves.js
+++ src/LegalMoves.js
@@ -28,51 +28,35 @@ export const isMoveLegal = (fromSquare, toSquare, squares) => {
     if (!isInMovingRange) return false;
 
     if (fromSquare.piece.name !== 'knight') {
-        const movePath = findPath(fromSquare, toSquare);
-        const fromSquares = movePath.map(_ => squares[_.rowId][_.columnId]);
-        
-        if (fromSquares.find(_ => _.piece)) return false;
+        const movePath = findPath(fromSquare, toSquare).map(_ => squares[_.rowId][_.columnId]);
+        if (movePath.find(_ => _.piece)) return false;
     }
 
     return true;
 }
 
-const isPawnMoveLegal = ({ rowsDiff, colsDiff, isForward }, { rowId: fromRow }, { piece, columnId: toColumn }, squares) => {
+const isPawnMoveLegal = ({ rowsDiff, colsDiff, isForward }, { rowId: fromRow, piece: fromPiece }, { piece: toPiece, columnId: toColumn }, squares) => {
     if (!isForward) return false;
     
     if (colsDiff === 1 && rowsDiff === 1) {
-        if (piece) return true;
+        if (toPiece) return true;
         
         const { piece: enPassantPiece } = squares[fromRow][toColumn];
 
         if (enPassantPiece?.lastMove) {
             const { name, lastMove: { fromSquare, toSquare } } = enPassantPiece;
-            if (name === 'pawn' && Math.abs(fromSquare.rowId - toSquare.rowId) === 2) return true;
+            if (name === 'pawn' && getMoveData(fromSquare, toSquare).rowsDiff === 2) return true;
         }
     }
     
-    const neverMoved = [1, 6].includes(fromRow);
+    const neverMoved = !fromPiece.lastMove;
     const maxDiff = neverMoved ? 2 : 1;
 
-    return !colsDiff && rowsDiff <= maxDiff && !piece;
+    return !colsDiff && rowsDiff <= maxDiff && !toPiece;
 }
 
-const isRookMoveLegal = ({ rowsDiff, colsDiff }) => {
-    return !colsDiff || !rowsDiff;
-}
-
-const isBishopMoveLegal = ({ rowsDiff, colsDiff }) => {
-    return colsDiff === rowsDiff;
-}
-
-const isKnightMoveLegal = ({ rowsDiff, colsDiff }) => {
-    return (colsDiff === 2 && rowsDiff === 1) || (rowsDiff === 2 && colsDiff === 1);
-}
-
-const isKingMoveLegal = ({ rowsDiff, colsDiff }) => {
-    return colsDiff <= 1 && rowsDiff <= 1;
-}
-
-const isQueenMoveLegal = (...args) => {
-    return isRookMoveLegal(...args) || isBishopMoveLegal(...args);
-}
+const isRookMoveLegal = ({ rowsDiff, colsDiff }) => !colsDiff || !rowsDiff;
+const isBishopMoveLegal = ({ rowsDiff, colsDiff }) => colsDiff === rowsDiff;
+const isKnightMoveLegal = ({ rowsDiff, colsDiff }) => (colsDiff === 2 && rowsDiff === 1) || (rowsDiff === 2 && colsDiff === 1);
+const isKingMoveLegal = ({ rowsDiff, colsDiff }) => colsDiff <= 1 && rowsDiff <= 1;
+const isQueenMoveLegal = (...args) => isRookMoveLegal(...args) || isBishopMoveLegal(...args);

868a075668ca9ddbb89b7cc4c8a451e2a5f29e22
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2022-11-03 10:18:25 +0100

    Imported CSS from App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import { ActionCreators } from "redux-undo";
 
+import "./index.css";
 import ChessBoard from "./ChessBoard";
 
 export default function App() {
-  const { gameState } = useSelector((state) => state.squares.present);
-  const dispatch = useDispatch();
+	const { gameState } = useSelector(state => state.squares.present);
+	const dispatch = useDispatch();
 
-  const onSomeDo = (isRedo) => () =>
-    dispatch((isRedo ? ActionCreators.redo : ActionCreators.undo)());
+	const onSomeDo = isRedo => () =>
+		dispatch((isRedo ? ActionCreators.redo : ActionCreators.undo)());
 
-  const onUndo = onSomeDo(false);
-  const onRedo = onSomeDo(true);
+	const onUndo = onSomeDo(false);
+	const onRedo = onSomeDo(true);
 
-  return (
-    <div>
-      <h1 style={{ color: "white" }}>State is: {gameState || "normal"}</h1>
+	return (
+		<div>
+			<h1 style={{ color: "white" }}>State is: {gameState || "normal"}</h1>
 
-      <div>
-        <button onClick={onUndo}>Back</button>
-        <button onClick={onRedo}>Forward</button>
-      </div>
+			<div>
+				<button onClick={onUndo}>Back</button>
+				<button onClick={onRedo}>Forward</button>
+			</div>
 
-      <div>
-        <ChessBoard></ChessBoard>
-      </div>
-    </div>
-  );
+			<div>
+				<ChessBoard></ChessBoard>
+			</div>
+		</div>
+	);
 }
